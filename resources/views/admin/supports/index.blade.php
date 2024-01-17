52ca0d69c2c49a96e97a7dd4d19f95cb77dbe0e4
Lucas P. Maciel <lukasptta@gmail.com>
2024-01-17 20:44:11 -0300

    update_web

---
+++ resources/views/admin/supports/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Pagina dos suportes</h1>
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use App\Http\Controllers\Site\siteController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\SupportController;
 
 
+Route::get('/supports',[SupportController::class,'index'])->name('supports.index');
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contact', [siteController::class,'contacto']);
+Route::get('/contact', [SiteController::class,'contacto']);
